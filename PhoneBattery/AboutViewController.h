4b9086a5af9d25e88c1d7063b46013c591a9f295
Marcel Voss <marcel.voss@me.com>
2015-09-22 17:07:33 +0200

    I ran into massive problems when using Swift for something so I switched to Objective-C.
    I know, this is a very suboptimal decision and I will eventually have to rewrite it again.

---
+++ PhoneBattery/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  PhoneBattery
+//
+//  Created by Marcel Voß on 21.09.15.
+//  Copyright © 2015 Marcel Voss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UITableViewController
+
+@end
